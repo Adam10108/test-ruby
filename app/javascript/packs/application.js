cf7d4e0cf8f5dcce8355c5d19724fc1fb227f1d1
Adam10108 <adam_atlas1998@hotmail.com>
2020-03-31 23:55:21 +0700

    fix:reinstall jquery materialize

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'materialize-css/dist/js/materialize';
 
 require('@rails/ujs').start();
-// require('jquery');
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
+require('jquery');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,7 +17,6 @@ require('channels');
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 //= require materialize
-//= require jquery
 
 $(document).on('turbolinks:load', function() {
   $('.dropdown-trigger').dropdown();
